52d10b9769538b161a2c080c08e5d93bededf768
Fauzan <fauzan.c4@gmail.com>
2025-02-22 07:15:48 +0700

    update USerCreateFormatter nomor_rekening -> number_balance

--- user/formatter.go
+++ user/formatter.go
@@ -1,7 +1,7 @@
 package user
 
 type UserCreateFormatter struct {
-	NumberBalance	string	`json:"nomor_rekening"`
+	NumberBalance	string	`json:"number_balance"`
 }
 
 type UserBalanceFormatter struct {
